465a6046a29a6d47e8fc20e65207e2ed1705f4e3
Syedhassan <syedhassan.alidrus1999@gmail.com>
2026-08-25 22:52:28 +0700

    ppp

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,11 +1,4 @@
 const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-console.log("☁️ Cloudinary cloud_name:", process.env.CLOUDINARY_CLOUD_NAME);
-
+// ไม่ต้อง config อะไร CLOUDINARY_URL จะถูกอ่านอัตโนมัติ
+console.log("☁️ Cloudinary cloud_name:", cloudinary.config().cloud_name);
 module.exports = cloudinary;
